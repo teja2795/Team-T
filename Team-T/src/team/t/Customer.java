946edcdb35b33c4e60b910d6e0c7bea471a441ce
1894438 <1894438@E21042.CEGEPGIM.LCL>
2019-02-09 10:36:05 -0500

    Customers Class Commit

---
+++ Team-T/src/team/t/Customer.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team.t;
+
+/**
+ *
+ * @author 1894438
+ */
+public class Customer {
+    String name, location;
+    
+    public void sendOrder(){
+        System.out.println("Send Order");
+        
+    }
+    public void receiveOrder(){
+        System.out.println("Recieve Order");
+    }
+}
