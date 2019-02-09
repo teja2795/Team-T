9b2c9de31cc11a858296dfa957d62b9f15702e7e
vikram.sumanth999@gmail.com <swaranbachu@gmail.com>
2019-02-09 11:06:20 -0500

    Normal  class commit

---
+++ Team-T/src/team/t/Normal.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package team.t;
+
+import java.util.Date;
+
+/**
+ *
+ * @author temp
+ */
+public class Normal {
+    Date date;
+    String number;
+    public void confirm()
+    {
+        System.out.println("this is confirm method in Normal order class");
+    }
+    public void close()
+    {
+        System.out.println("this is close method in Normal order class");
+    }
+    public void dispatch()
+    {
+        System.out.println("this is dispatch method in Normal order class");
+    }
+    public void receive()
+    {
+        System.out.println("this is receive method in Normal order class");
+    }
+}
